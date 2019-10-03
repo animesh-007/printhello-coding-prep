15e46eb9c5d90cabe4aee2b1d91f548f53e06b08
animesh-007 <animeshagarwal7@gmail.com>
2019-10-04 03:36:20 +0530

    deleting

---
+++ ques_3.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+    int n=4;
+    int i=0;
+    do{
+        printf("-----------------------PrintHello-------------------------------\n");
+        int k = n*n/n-n+n/n;
+        while(k<2){
+            printf("\nKickstart your programming skills with C/C++\n\n");
+            for(i=k-1;i<1;i++){
+                        printf("C/C++ is the first language to learn because it not only helps you to strengthen \n"
+        " your programming basics but its logic building techniques help you to excel in coding life. \n");
+                switch(i){
+                    case (4^4):
+                        printf("->Live online classes from experts\n");
+                        
+                    case (5%2):
+                        printf("->Awesome Projects thats will strengthen your grip on language.\n");
+                    case (5*6/2-2*6-1):
+                        printf("->Assignments that will blow your mind and coming out a awesome coder out of it.\n\n");
+                        
+                    default:
+                        printf("----Register now on www.printhello.in/programming------\n\n");
+                        printf("Classes are starting from 8th October onwards.");
+                        
+                }
+            }
+            k++;
+        }
+    }while(n!=4);
+    return 0;
+}
